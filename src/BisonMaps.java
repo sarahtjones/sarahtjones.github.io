6de9a26f73c2682ca1eb2adefc0bf694116f10d4
Sarah <saraht.jones8@gmail.com>
2014-04-01 21:26:47 -0400

    first push

---
+++ src/BisonMaps.java
@@ -0,0 +1,36 @@
+
+
+
+
+/*************************************************************************
+ *  Compilation:  javac BisonMaps.java
+ *  Execution:    java BisonMaps mapfile < input.txt
+ *  Dependencies: EuclideanGraph.java Dijkstra.java In.java StdIn.java
+ *
+ *  Reads in a map from a file, and repeatedly reads in two integers s
+ *  and d from standard input, and prints the shortest path from s
+ *  to d to standard output.
+ *
+ ****************************************************************************/
+
+
+public class BisonMaps {
+
+    public static void main(String[] args) {
+
+        // read in the graph from a file
+        In graphin = new In("mapfile.txt");
+        EuclideanGraph G = new EuclideanGraph(graphin);
+        System.err.println("Done reading the graph " + "mapfile.txt" );
+        System.err.println("Enter query pairs from stdin");
+
+        // read in the s-d pairs from standard input
+        Dijkstra dijkstra = new Dijkstra(G);
+        while(!StdIn.isEmpty()) {
+            int s = StdIn.readInt();
+            int d = StdIn.readInt();
+            dijkstra.showPath(s, d);
+            System.out.println();
+        }
+    }
+}
